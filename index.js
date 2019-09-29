49bd9d870097b884c1bc3444147a0cb90c31b684
Art Navsegda <art@debian>
2019-09-30 01:28:43 +0300

    ee boy

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
-const app = express()
 const port = process.env.PORT || 5000;
+const puppeteer = require('puppeteer');
 
 express()
   .use(express.static('public'))
   .get('/', (req, res) => res.send('Hello World!'))
+  .get('/genpdf', (req, res) => {
+    (async() => {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+
+        const page = await browser.newPage();
+        await page.goto('https://example.com');
+        await page.screenshot().then(function(buffer) {
+            res.setHeader('Content-Disposition', 'attachment;filename="' + 'jeeson' + '.png"');
+            res.setHeader('Content-Type', 'image/png');
+            res.send(buffer)
+        });
+
+        await browser.close();
+    })();
+  })
   .listen(port, () => console.log(`Example app listening on port ${port}!`))

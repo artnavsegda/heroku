209ec199a090178a74d84fa1c1ad37bdc1a35003
Art Navsegda <art@debian>
2019-09-30 00:58:02 +0300

    express

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
 
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
